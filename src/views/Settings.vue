f7aba00c16fb5d8e6c2abe53ba0e88204b5ee95e
Gaurav V <gv71.msrit@gmail.com>
2020-07-14 20:21:11 +0530

    midway done with settings page

--- src/views/Settings.vue
+++ src/views/Settings.vue
@@ -8,20 +8,93 @@
       </div>
     </div>
     <div class="line"></div>
-    <div class="card">
+    <div class="card-container">
+      <div class="card">
         <div class="title">
-            <img src="../assets/Settings/account.svg" alt="">
-            <h3>Account Details</h3>
+          <img src="../assets/Settings/account.svg" alt />
+          <h3>Account Details</h3>
         </div>
         <div class="body">
-
+          <div class="container">
+            <div class="flex">
+              <img src="../assets/Settings/name.svg" alt="name" />
+              <h3>User Name</h3>
+            </div>
+            <div v-if="isEditingName" class="flex">
+              <input type="text" v-model="name" placeholder="Enter Name" />
+              <img
+                @click="isEditingName=false"
+                src="../assets/Common/save.svg"
+                alt="save"
+                style="margin-left: auto;width: 30px;cursor: pointer;"
+                title="Save changes"
+              />
+              <img
+                @click="isEditingName=false"
+                src="../assets/Common/cancel.svg"
+                alt="cancel"
+                style="width: 30px;cursor: pointer;"
+                title="Cancel edit"
+              />
+            </div>
+            <div class="flex" v-else>
+              <div class="box">{{name}}</div>
+              <img
+                src="../assets/Common/edit.svg"
+                alt="edit"
+                title="Edit user name"
+                style="margin-left: auto;width:39px;cursor:pointer;"
+                @click="isEditingName=true"
+              />
+            </div>
+          </div>
+          <div class="line" style="margin: 0.5rem 0 1rem 0;"></div>
+          <div class="container">
+            <div class="flex">
+              <img src="../assets/Settings/gmail.svg" alt="email" />
+              <h3>Email</h3>
+            </div>
+            <div style="margin: 0.3rem 0;">{{email}}</div>
+          </div>
+          <div class="line"></div>
+          <div class="btn-container">
+            <div class="btn" @click="route">Reset Password</div>
+          </div>
         </div>
+      </div>
+      <div class="card">
+        <div class="title">
+          <img src="../assets/Settings/data.svg" alt />
+          <h3>Data Collected</h3>
+        </div>
+        <div class="body">lol</div>
+      </div>
+      <div class="card">
+        <div class="title">
+          <img src="../assets/Settings/privacy.svg" alt />
+          <h3>User Privacy</h3>
+        </div>
+        <div class="body">
+          <p style="line-height: 1.15;">Privacy is built in from the beginning. Our products and features include innovative privacy technologies and techniques designed to minimise how much of your data we — or anyone else — can access. And powerful security features help prevent anyone except you from being able to access your information.</p>
+          <div class="line" style="margin: 1rem 0;"></div>
+          <div class="btn-container">
+            <div class="btn" @click="route">Delete Account</div>
+          </div>
+        </div>
+      </div>
     </div>
   </div>
 </template>
 
 <script>
 export default {
+  data() {
+    return {
+      name: "Aravind Ramesh Shreyas ",
+      email: "halalcut@shreyas.com",
+      isEditingName: false
+    };
+  },
   methods: {
     route() {
       this.$router.push("/reset");
@@ -44,7 +117,7 @@ export default {
   .heading-container {
     height: 100%;
     width: 100%;
-      padding: .7rem 0 1rem 1rem;
+    padding: 0.7rem 0 1rem 1rem;
     display: flex;
     justify-content: flex-start;
     align-items: center;
@@ -52,7 +125,7 @@ export default {
       width: 60px;
       margin-right: 0.5rem;
     }
-    .heading {      
+    .heading {
       h1,
       h4 {
         margin: 0.5rem;
@@ -68,31 +141,99 @@ export default {
     //background-image: $gradient;
     margin: 0 0 1rem 0;
   }
-  .card{                 
+  .card-container {
+    display: flex;
+    justify-content: center;
+    flex-wrap: wrap;
+    .card {
+      width: 23rem;
+      height: 23rem;
+      margin: 1.5rem;
       background: white;
-      box-shadow: 0 0px 10px rgba(0, 0, 0, 0.25);
+      border-radius: 1rem;
+      overflow: hidden;
+      box-shadow: 0 14px 28px rgba(0, 0, 0, 0.25),
+        0 10px 10px rgba(0, 0, 0, 0.22);
       display: flex;
       flex-direction: column;
-      justify-content: center;      
-      border-radius: 0.8rem;
-      .title{
-          padding: 1rem;
-          display: flex;
-          flex-direction: column;
-          align-items: center;
-          background: $primary-light;
-         // background-image: $gradient;
-          border-top-left-radius: 0.8rem;
-          border-bottom-left-radius: 0.8rem;
-          img{
-              width: 80px;
-          }
-          h3{
-              margin:0.8rem;
-              color: white;
-          }
-
+      .title {
+        widows: 100%;
+        height: 3rem;
+        padding: 1rem;
+        display: flex;
+        align-items: center;
+        justify-content: center;
+        background-image: $gradient;
+        img {
+          width: 50px;
+        }
+        h3 {
+          color: white;
+          margin: 0 1rem;
+        }
       }
+      .body {
+        padding: 1rem;
+        .container {
+          h3 {
+            margin: 0;
+            color: black;
+            font-weight: lighter;
+          }
+          input,
+          .box {
+            background: rgba(orange, 0.3);
+            border: 1px solid grey;
+            padding: 12px 15px;
+            margin: 10px 0;
+            width: 13rem;
+          }
+          .box {
+            width: 15rem;
+            background: rgba($primary, 0.3);
+            font-size: small;
+          }
+          .flex {
+            height: 100%;
+            display: flex;
+            justify-content: flex-start;
+            align-items: center;
+            color: black;
+            img {
+              width: 32px;
+              margin: 0 0.5rem 0 0;
+            }
+          }
+        }
+        .btn-container {
+          display: flex;
+          justify-content: center;
+          align-items: center;
+          .btn {
+            margin: 0.2rem;
+            text-align: center;
+            border-radius: 20px;
+            border: 1px solid $primary-light;
+            background-color: $primary-light;
+            background-image: $gradient;
+            color: #ffffff;
+            font-size: 12px;
+            font-weight: bold;
+            padding: 12px 45px;
+            letter-spacing: 1px;
+            text-transform: uppercase;
+            transition: transform 80ms ease-in;
+            cursor: pointer;
+          }
+          .btn:active {
+            transform: scale(0.95);
+          }
+          .btn:focus {
+            outline: none;
+          }
+        }
+      }
+    }
   }
 }
 </style>
